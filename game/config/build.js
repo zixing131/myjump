0d3ca75b8e483f6d8af83ceb67b8dc154e7b1bc5
紫星 <1311817771@qq.com>
2023-02-17 17:28:49 +0800

    no commit message

--- game/config/build.js
+++
@@ -1,4 +0,0 @@
-// generated, build-specific configuration
-
-config.release = false;
-config.version = "1659142910";
--- game/config/runtests.js
+++
@@ -1,7 +0,0 @@
-config.jars = "jar/jl.jar";
-config.jad = "";
-config.midletClassName = "GameMIDlet";
-
-MIDlet.shouldStartBackgroundService = function() {
-  return fs.exists("/startBackgroundService");
-};
